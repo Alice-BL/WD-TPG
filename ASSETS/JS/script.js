398e9455c26cf79b5551e01e170688e5e0c31c22
Thu Phan <alicebe.phan@gmail.com>
2021-09-18 01:25:02 -0500

    created an API key

--- ASSETS/JS/script.js
+++ ASSETS/JS/script.js
@@ -1,7 +1,11 @@
+// created an API key
+var weatherAPIKey = "4a9646ee83d600552d65c84deed82d99";
+
+
 // write a function to getWeather(). I will pass in a city as an argument to getWeather(city).
 function getWeather(city) {
     //Inside getWeather make a fetch request that returns the data from the openweathermap API.
-    fetch('https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}', {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q={Dallas}&appid={API key}', {
         method: 'GET',
         credentials: 'same-origin',
         redirect: 'follow'
