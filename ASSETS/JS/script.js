4f5f621e5f53cf0b0659118b6dabcef7ab85f53d
unknown <alicebe.phan@gmail.com>
2021-12-26 22:19:28 -0600

    set addEventListener and tested api key, got weather data in console

--- ASSETS/JS/script.js
+++ ASSETS/JS/script.js
@@ -1,6 +1,6 @@
 // created an API key
 var weatherAPIKey = "4a9646ee83d600552d65c84deed82d99";
-var cityInput = document.querySelector('#city-input');
+var cityInput = document.querySelector('#City-input');
 var searchButton = document.querySelector('#search-Btn');
 var currentForecast = document.querySelector('.current-forecast');
 var fiveDay = document.querySelector('.FiveDay');
@@ -9,21 +9,23 @@ var fiveDay = document.querySelector('.FiveDay');
 // write a function to getWeather(). I will pass in a city as an argument to getWeather(city).
 function getWeather(city) {
     //Inside getWeather make a fetch request that returns the data from the openweathermap API.
-    fetch('https://api.openweathermap.org/data/2.5/weather?q={Dallas}&appid={API key}', {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherAPIKey}&units=imperial`, {
         method: 'GET',
         credentials: 'same-origin',
         redirect: 'follow'
 })
 
     .then(function(response){
-        return response.JSON();
+        return response.json();
     })
     .then(function(data){
         console.log(data);
     });
 }
 
-
+searchButton.addEventListener('click', function() {
+    getWeather(cityInput.value);
+})
 
 // Write a currentWeather(data) function that returns html values based on the data that is returned in your getWeather() fetch request.
 
